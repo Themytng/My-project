61f14fdeec414749908b50386f51aaa7f12cc5b6
Themytng <144147102+Themytng@users.noreply.github.com>
2023-09-12 12:11:25 -0400

    New code, I guess

--- Assets/New Folder/Paddle1.cs
+++ Assets/New Folder/Paddle1.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Paddle1 : MonoBehaviour
 {
     //Variables for movement
-    public float speed = 1f;
+    public float speed = .001f;
     public float yBorder = 6f;
 
     // Start is called before the first frame update
@@ -17,13 +17,16 @@ public class Paddle1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.O))
+        if (Input.GetKey(KeyCode.W) && transform.position.y < yBorder) //when O is pressed
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed); //move up
         }
-        if (Input.GetKey(KeyCode.L))
+
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -yBorder)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y - speed);
         }
     }
+
+    
 }
--- Assets/New Folder/ball.cs
+++ Assets/New Folder/ball.cs
@@ -4,42 +4,82 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
-    public float xSpeed = 0;
-    public float ySpeed = 0;
+    public float xSpeed = 0; //Horizontal speed
+    public float ySpeed = 0; //vertical speed
 
+    //Border variable
     private float xBorder = 9f;
-    private float yBorder = 5f;
-    private float xNegBorder = -7.5f;
-    private float yNegBorder = -5f;
+    private float yBorder = 4f;
+    private float xNegBorder = -9f;
+    private float yNegBorder = -4f;
+
+    //variable for move state
+    public bool xMove = true;
+    public bool yMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
         xSpeed = 0.010f;
         ySpeed = 0.010f;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y + ySpeed);
+        if (xMove)
+        {
+            transform.position = new Vector2(transform.position.x + xSpeed, transform.position.y + ySpeed); //move right
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - xSpeed, transform.position.y + ySpeed);
+        }
+
+        if (yMove)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + ySpeed); //up
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - ySpeed);
+        }
+
+
 
         if (transform.position.x >= xBorder) //in order, from top to bottom; left, right,down, and up movements.
         {
-            xSpeed = -xSpeed;
+            xMove = false;
         }
         if (transform.position.x <= xNegBorder)
         {
-            xSpeed = -xSpeed;
+            xMove = false;
         }
         if (transform.position.y >= yBorder)
         {
-            ySpeed = -ySpeed;
+            ySpeed *= -1;
         }
         if (transform.position.y <= yNegBorder)
         {
-            ySpeed = -ySpeed;
+            ySpeed *= -1;
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision) //when a collision happens)
+    {
+        Debug.Log("hit");
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (xMove == true)
+            {
+                xMove = false;
+            }
+            else
+            {
+                xMove = true;
+            }
+        }
+    }
+
 }
